ba78d300c2d3a19fd7c733a5b23238589729bead
Johnson Li <johnsonli1993@163.com>
2016-08-31 17:19:38 +0800

    yaml config

---
+++ hurricane/config/__init__.py
@@ -0,0 +1,4 @@
+import yaml
+
+main_yaml = 'resource/main.yml'
+conf = yaml.load(open(main_yaml))
--- hurricane/config/db_config.py
+++ hurricane/config/db_config.py
@@ -1,5 +1,17 @@
-import db.impl.sqlite.client
+import importlib
+
+import config
+
+db_conf = config.conf.get('db', None)
 
 
 def get_client():
-    return db.impl.sqlite.client.Client()
+    module_str = db_conf['module']
+    class_str = db_conf['class']
+
+    if not module_str.startswith('db.impl.'):
+        raise AttributeError('db client should be located in db.impl: ' + module_str)
+
+    module = importlib.import_module(module_str)
+    cls = getattr(module, class_str)
+    return cls()
